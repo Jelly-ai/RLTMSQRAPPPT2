2e6cfe9cee0178003c08a43f662b210ac8c922c8
Jelly-ai <73816313+Jelly-ai@users.noreply.github.com>
2021-05-17 14:37:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560 , 150);
+    
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is initialized");
+}
+
+function gotPoses(results){
+    if(results.lenght > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX=" + noseX + "noseY=" + noseY );
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("left wrist X=" + leftWristX + "right wrist X=" + rightWristX)
+    }
+}
+function draw(){
+    background("#D3D3D3");
+    document.getElementById("square_sides").innerHTML = "width and height of the square will be=" + difference + "px";
+    fill("#DC143C");
+    stroke("#DC143C");
+    square(noseX , noseY , difference);
+}
